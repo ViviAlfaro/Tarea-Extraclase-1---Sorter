841ae7906321e16646f2b28b54705fc06f9fb35d
ViviAlfaro <alfaroviviana2004 @gmail.com>
2024-08-06 19:47:47 -0600

    mostrar los numeros enteros con el formato separados por comas

--- lectura.cpp
+++ lectura.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) { //funcion principal 
     if (argc != 2) {
         cerr << "Uso: " << argv[0] << " <nombre" << endl;
         return 1;
@@ -13,7 +13,24 @@ int main(int argc, char* argv[]) {
     const char* FILENAME = argv[1];
     ifstream inFile(FILENAME, ios::binary);
     if (!inFile) {
-        cerr << "Error: No se pudo abrir el archivo de lectura" << endl;
+        cerr << "Error: No se pudo abrir el archivo de lectura" << endl; //mensaje de error en caso de que no se cumpla
         return 1;
     }
- }
+ 
+    // lee y muestra los números enteros con formato
+    int num;
+    bool first = true;
+    while (inFile.read(reinterpret_cast<char*>(&num), sizeof(num))) { //lee datos del archivo en bloques del tamaño de un entero
+        if (!first) {
+            cout << ", ";
+        }
+        cout << num;
+        first = false;
+    }
+
+    // cierra el archivo
+    inFile.close();
+    cout << endl;
+
+    return 0;
+}
